56942ae27d2442cb44bd21727be291071d2e96ac
ad4133 <ad4133@pleasantonusd.net>
2025-03-27 21:40:25 +0000

    s

---
+++ src/Giggle.java
@@ -0,0 +1,6 @@
+public class Giggle extends Post {
+    
+
+
+
+}
---
+++ src/Post.java
@@ -0,0 +1,8 @@
+public class Post {
+    private String dateCreated;
+    private String creator;
+    private String title;
+    private String description;
+    private int viewsNum;
+    private ArrayList<Comment> comments;
+}
