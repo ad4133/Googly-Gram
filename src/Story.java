bb0374a5cf0a3a994fc2a11bd24ab01d4fbbc664
ad4133 <ad4133@pleasantonusd.net>
2025-04-28 21:02:49 +0000

    s

--- src/Story.java
+++ src/Story.java
@@ -1,5 +1,4 @@
 public class Story extends Post {
-    private 
     
     public Story(String creator, String title, String description, String altText, String dateCreated, int views, int likes) {
         super(creator, title, description, altText, dateCreated, views, likes);
